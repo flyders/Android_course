9875331b02e0787e724d84eb2ff6e2f057510683
Georgi Mihaylov <cocomen@gmail.com>
2015-02-11 21:06:11 +0200

    Test

---
+++ FizzBuzz/script.js
@@ -0,0 +1,17 @@
+
+var FizzBuzz = function() {
+
+    for(i=0; i< 100; i++ ) {
+
+        if (i % 3 ===0 ) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else if (i % 5 === 0 && i % 3 === 0) {
+            console.log('FizzBuzz');
+        }
+    }
+
+    return FizzBuzz();
+};
+
